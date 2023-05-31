9df24966494766b15c6791624edc1d7e3aacac3b
sashaspasojevic <spsjvcsasa@yahoo.com>
2023-05-31 14:36:58 +0200

    add some funcionality to navbar

--- src/componets/Navbar.js
+++ src/componets/Navbar.js
@@ -1,7 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
+import Wrapper from "../assets/wrappers/Navbar";
+import { FaAlignLeft, FaUserCircle, FaCaretDown } from "react-icons/fa";
+import { useDispatch, useSelector } from "react-redux";
+import Logo from "./Logo";
+import { toggleSidebar } from "../features/user/userSlice";
 
 const Navbar = () => {
-  return <h2>Navbar</h2>;
+  const [showLogout, setShowLogout] = useState(false);
+  const { user } = useSelector((store) => store.user);
+  const dispatch = useDispatch();
+
+  const toggle = () => {
+    dispatch(toggleSidebar());
+  };
+
+  return (
+    <Wrapper>
+      <div className='nav-center'>
+        <button type='button' className='toggle-btn' onClick={toggle}>
+          <FaAlignLeft />
+        </button>
+        <div>
+          <Logo />
+          <h3 className='logo-text'>dashboard</h3>
+        </div>
+        <div className='btn-container'>
+          <button
+            type='button'
+            className='btn'
+            onClick={() => {
+              setShowLogout(!showLogout);
+            }}
+          >
+            <FaUserCircle />
+            {user?.name}
+            <FaCaretDown />
+          </button>
+          <div className={showLogout ? "dropdown show-dropdown" : "dropdown"}>
+            <button
+              type='button'
+              className='dropdown-btn'
+              onClick={() => {
+                console.log("logout user");
+              }}
+            >
+              logout
+            </button>
+          </div>
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default Navbar;
